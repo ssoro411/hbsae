6ca0301784566b535608882732f065d7bcf41b58
ssoro411 <31112051+ssoro411@users.noreply.github.com>
2019-12-03 21:11:29 -0500

    Add files via upload

---
+++ R/hbsae-package.R
@@ -0,0 +1,16 @@
+#' The 'hbsae' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name hbsae-package
+#' @aliases hbsae
+#' @useDynLib hbsae, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2019). RStan: the R interface to Stan. R package version 2.19.2. https://mc-stan.org
+#'
+NULL
